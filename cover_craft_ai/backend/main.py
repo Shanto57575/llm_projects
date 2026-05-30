b82f1462c578c229e1ac365bdb78005d0312680d
Md. Shahidul Islam <shanto57575@gmail.com>
2026-05-30 19:53:39 +0600

    added frontend live url for cors config

--- cover_craft_ai/backend/main.py
+++ cover_craft_ai/backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "https://cover-craft-ai.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
